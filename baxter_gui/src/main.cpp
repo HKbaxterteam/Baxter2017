a62ab28f532d76edbce84b63599d2eeb7d3da869
n3dinex <nadine.drollinger@gmail.com>
2017-07-31 11:49:45 +0200

    first gui

--- baxter_gui/src/main.cpp
+++ baxter_gui/src/main.cpp
@@ -1,12 +1,32 @@
-#include <ros/ros.h>
-#include "baxter_gui.h"
+/**
+ * @file /src/main.cpp
+ *
+ * @brief Qt based gui.
+ *
+ * @date November 2010
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui>
 #include <QApplication>
+#include "../include/baxter_gui/main_window.hpp"
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Baxter_gui w;
+/*****************************************************************************
+** Main
+*****************************************************************************/
+
+int main(int argc, char **argv) {
+
+    /*********************
+    ** Qt
+    **********************/
+    QApplication app(argc, argv);
+    baxter_gui::MainWindow w(argc,argv);
     w.show();
+    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+    int result = app.exec();
 
-    return a.exec();
+	return result;
 }
