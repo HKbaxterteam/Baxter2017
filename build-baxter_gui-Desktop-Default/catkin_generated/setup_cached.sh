84bcf76fc4e21cebc6bc57a157cf93b938981437
n3dinex <nadine.drollinger@gmail.com>
2017-08-01 08:18:55 +0200

    game_master server

--- build-baxter_gui-Desktop-Default/catkin_generated/setup_cached.sh
+++ build-baxter_gui-Desktop-Default/catkin_generated/setup_cached.sh
@@ -8,9 +8,11 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default/devel/include:$CPATH"
 export LD_LIBRARY_PATH="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default/devel/lib:$LD_LIBRARY_PATH"
-export PATH="/opt/ros/indigo/bin:/usr/lib/x86_64-linux-gnu/qt4/bin:/usr/bin:/opt/Qt5.7.0/Tools/QtCreator/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PATH="/opt/ros/indigo/bin:/usr/lib/x86_64-linux-gnu/qt4/bin:/usr/bin:/usr/lib/x86_64-linux-gnu/qt4/bin:/usr/bin:/opt/Qt5.7.0/Tools/QtCreator/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 export PKG_CONFIG_PATH="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default"
+export PYTHONPATH="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/n/catkin_ws/src/Baxter2017/build-baxter_gui-Desktop-Default/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/n/catkin_ws/src/Baxter2017/baxter_gui:$ROS_PACKAGE_PATH"
