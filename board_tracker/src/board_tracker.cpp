81b7f7c65e8ab6e35a4941227dbf465f92d74522
MWelle77 <mwelle@kth.se>
2017-08-15 19:51:02 +0800

    problems with warping

--- board_tracker/src/board_tracker.cpp
+++ board_tracker/src/board_tracker.cpp
@@ -51,12 +51,12 @@ class board_cutout
     bool debug_flag;
     //Open cv images
     Mat org, grey, game; 
-    Mat M, rotated, cropped;
+    Mat M, rotated,rotated2, cropped;
     //board_cutout() : it_(n_),rboard_width(100),rboard_height(100),rqrcode_width(20), rqrcode_height(20), rqrboard_offset_x(-20), rqrboard_offset_y(30), debug_flag(false)
-    board_cutout() : it_(n_),rboard_width(80),rboard_height(80),rqrcode_width(20), rqrcode_height(20), rqrboard_offset_x(0), rqrboard_offset_y(0), debug_flag(false)
+    board_cutout() : it_(n_),rboard_width(420),rboard_height(420),rqrcode_width(90), rqrcode_height(90), rqrboard_offset_x(-400), rqrboard_offset_y(380), debug_flag(false)
     {
       // Subscrive and publisher
-      image_sub_raw_ = it_.subscribe("/kinect2/hd/image_color", 1, &board_cutout::imageCb, this);
+      image_sub_raw_ = it_.subscribe("/usb_cam/image_raw", 1, &board_cutout::imageCb, this);
       image_pub_cut_ = it_.advertise("/TTTgame/cut_board", 1);
       
       //debug
@@ -74,6 +74,8 @@ class board_cutout
         destroyWindow("Cut output"); 
       }    
     }
+
+
 
     void imageCb(const sensor_msgs::ImageConstPtr& msg)
     {
@@ -136,7 +138,7 @@ class board_cutout
             iboard_width=(rboard_width*iqrcode_height)/rqrcode_width;
             iboard_height=(rboard_height*iqrcode_height)/rqrcode_height;
             //set 4 points to cut out the image
-            vpgame.push_back(vp[0]+Point(iqrboard_offset_x,iqrboard_offset_x));
+            vpgame.push_back(vp[0]+Point(iqrboard_offset_x,iqrboard_offset_y));
             vpgame.push_back(vpgame[0]+Point(iboard_width,0));
             vpgame.push_back(vpgame[0]+Point(iboard_width,-iboard_height));
             vpgame.push_back(vpgame[0]+Point(0,-iboard_height));
@@ -149,13 +151,174 @@ class board_cutout
                 angle += 90.0;
                 swap(rect_size.width, rect_size.height);
             }
-            // get the rotation matrix
-            M = getRotationMatrix2D(rgame.center, angle, 1.0);
+            // get the rotation matrix 
+            M = getRotationMatrix2D(rgame.center, angle, 1);
             // perform the affine transformation
             warpAffine(org, rotated, M, org.size(), INTER_CUBIC);
+
+            //take care of z rotation
+
+            /*
+            double phi=0.0;
+            double theta=0.00;
+            double psi=0.04;
+
+            Mat D = (Mat_<double>(3, 3) <<
+              cos(phi), sin(phi),           0,
+              -sin(phi), cos(phi), 0,
+              0, 0,  1);
+
+            Mat C = (Mat_<double>(3, 3) <<
+              1, 0, 0,
+              0, cos(theta), sin(theta),
+              0, -sin(theta),  cos(theta));
+
+            Mat B = (Mat_<double>(3, 3) <<
+              cos(psi), sin(psi),           0,
+              -sin(psi), cos(psi), 0,
+              0, 0,  1);
+
+              */
+
+/*
+
+            double roll=0.4;
+            double pitch=0.00;
+            double yaw=0.0;
+
+
+            Mat Rx = (Mat_<double>(3, 3) <<
+              1,          0,           0,
+              0, cos(roll), -sin(roll),
+              0, sin(roll),  cos(roll));
+
+            Mat Ry = (Mat_<double>(3, 3) <<
+              cos(pitch), 0, sin(pitch),
+              0, 1,          0,
+              -sin(pitch), 0,  cos(pitch));
+
+            Mat Rz = (Mat_<double>(3, 3) <<
+              cos(yaw), -sin(yaw), 0,
+              sin(yaw),  cos(yaw), 0,
+              0,          0,           1);
+              
+
+cout << "more matrix " << endl;     
+          
+
+            Mat R = Rx*Rz*Ry;
+            //Mat R = B*C*D;
+            double rotwidth=rotated.cols;
+            double rotheight=rotated.rows;
+            Mat T = (Mat_<double>(3, 1) << 0,0,1);
+            Mat H = (Mat_<double>(3, 1) << rotwidth/2,rotheight/2,1);
+            cout << "more T " << endl;  
+            T=R*H;
+cout << "more H2 " << endl; 
+            Mat H2 = (Mat_<double>(3, 3) <<
+              1, 0, (rotwidth/2-T.at<double>(0,0)),
+              0,  1, (rotheight/2-T.at<double>(1,0)),
+              0,          0,           1);
+cout << "more W " << endl; 
+              Mat W=H2*R;
+
+cout << "more warp " << H2 << endl; 
+
+            Mat HOPE = (Mat_<double>(2, 3) <<
+              W.at<double>(0,0), W.at<double>(0,1), 0, 
+              W.at<double>(1,0),  W.at<double>(1,1) ,0);
+            //Mat trans = A*R*A.inv();
+            //warpPerspective(rotated, rotated2, HOPE, rotated.size(),INTER_CUBIC);
+            //perspectiveTransform(rotated,rotated2,W);
+            warpAffine(rotated,rotated2,HOPE,rotated.size(),INTER_CUBIC);
+
+            
+/*
+            double roll=0.01;
+            double pitch=0.0;
+            double yaw=0.4;
+            double f=1;
+            double cx=0;
+            double cy=0;
+
+            // Camera Calibration Intrinsics Matrix
+    Mat A2 = (Mat_<double>(3,4) <<
+              f, 0, cx, 0,
+              0, f, cy, 0,
+              0, 0, 1,  0);
+    // Inverted Camera Calibration Intrinsics Matrix
+    Mat A1 = (Mat_<double>(4,3) <<
+              1/f, 0,   -cx/f,
+              0,   1/f, -cy/f,
+              0,   0,   0,
+              0,   0,   1);
+    // Rotation matrices around the X, Y, and Z axis
+    Mat RX = (Mat_<double>(4, 4) <<
+              1, 0,         0,          0,
+              0, cos(roll), -sin(roll), 0,
+              0, sin(roll), cos(roll),  0,
+              0, 0,         0,          1);
+    Mat RY = (Mat_<double>(4, 4) <<
+              cos(pitch),  0, sin(pitch), 0,
+              0,           1, 0,          0,
+              -sin(pitch), 0, cos(pitch), 0,
+              0,           0, 0,          1);
+    Mat RZ = (Mat_<double>(4, 4) <<
+              cos(yaw), -sin(yaw), 0, 0,
+              sin(yaw),  cos(yaw), 0, 0,
+              0,          0,       1, 0,
+              0,          0,       0, 1);
+    // Translation matrix
+    double dx=0;
+    double dy=0;
+    double dz=1;
+
+    Mat T = (Mat_<double>(4, 4) <<
+             1, 0, 0, dx,
+             0, 1, 0, dy,
+             0, 0, 1, dz,
+             0, 0, 0, 1);
+    // Compose rotation matrix with (RX, RY, RZ)
+    Mat R = RZ * RY * RX;
+    // Final transformation matrix
+    Mat H = A2 * (T * (R * A1));
+    // Apply matrix transformation
+    warpPerspective(rotated, rotated2, H, rotated.size(), INTER_LANCZOS4);
+
+*/
+
+
+    Point2f srcTri[3];
+   Point2f dstTri[3];
+
+   /// Set your 3 points to calculate the  Affine Transform
+   double gamma=0.1;
+   srcTri[0] = Point2f( 541, 458 );
+   srcTri[1] = Point2f( 476, 310 );
+   srcTri[2] = Point2f( 294, 315);
+
+   dstTri[0] = Point2f( 541, 458);
+   dstTri[1] = Point2f( 541,   310-100 );
+   dstTri[2] = Point2f( 295-100,  310-100 );
+
+   Mat warp_mat( 2, 3, CV_32FC1 );
+
+   warp_mat = getAffineTransform( srcTri, dstTri );
+
+   warpAffine( rotated, rotated2, warp_mat, rotated.size() );
+
+
+cout << "more done " << warp_mat << endl; 
+            namedWindow("rot", CV_WINDOW_AUTOSIZE);
+            namedWindow("org", CV_WINDOW_AUTOSIZE);
+            namedWindow("crop", CV_WINDOW_AUTOSIZE);
+            imshow("rot", rotated2); //show the frame in "MyVideo" window
+            imshow("org", rotated); //show the frame in "MyVideo" window
+
             // crop the resulting image
-            getRectSubPix(rotated, rect_size, rgame.center, cropped);
+            getRectSubPix(rotated2, rect_size, rgame.center, cropped);
             waitKey(10);
+            imshow("crop", cropped); //show the frame in "MyVideo" window
             //debug output
             if(debug_flag){
               imshow("Cut output", cropped); //show the frame in "MyVideo" window
