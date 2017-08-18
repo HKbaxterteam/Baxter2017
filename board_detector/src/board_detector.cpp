3618336031ad4f4e199dcf1e45ceaa9ae6cbae85
MWelle77 <mwelle@kth.se>
2017-08-18 14:46:43 +0800

    board detector works

--- board_detector/src/board_detector.cpp
+++ board_detector/src/board_detector.cpp
@@ -20,8 +20,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/opencv.hpp"
-//#include "opencv2/highgui/highgui.hpp"
-//#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,18 +40,8 @@ class board_detector
     int rows;
     int cols;
     int** gameboard;
-    int filter_red_r_low;
-	int filter_red_g_low;
-	int filter_red_b_low;
-	int filter_red_r_high;
-	int filter_red_g_high;
-	int filter_red_b_high;
-	int filter_blue_r_low;
-	int filter_blue_g_low;
-	int filter_blue_b_low;
-	int filter_blue_r_high;
-	int filter_blue_g_high;
-	int filter_blue_b_high;
+    int r_threshold;
+    int b_threshold;
 	//opencv help
 	vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
@@ -62,7 +50,7 @@ class board_detector
 	Mat filter_image_red, filter_image_blue;
 	Mat canny_output, drawing, countourtest;
 
-  board_detector() : it_(n_), debug_flag(true), rows(7), cols(7), filter_red_r_low(0), filter_red_g_low(0), filter_red_b_low(0), filter_red_r_high(255), filter_red_g_high(25), filter_red_b_high(25), filter_blue_r_low(0), filter_blue_g_low(0), filter_blue_b_low(50), filter_blue_r_high(25), filter_blue_g_high(25), filter_blue_b_high(255) 
+  board_detector() : it_(n_), debug_flag(true), rows(7), cols(7), r_threshold(110),b_threshold(110) 
   {
   	// Subscrive and publisher
     image_sub_cutout_ = it_.subscribe("/TTTgame/cut_board", 1, &board_detector::imageCb, this);
@@ -75,19 +63,21 @@ class board_detector
     //debug
     if(debug_flag){
       namedWindow("Input_cutout", CV_WINDOW_AUTOSIZE);
-      namedWindow("ROI-section", CV_WINDOW_AUTOSIZE);
-      namedWindow("ROI-section-filtered", CV_WINDOW_AUTOSIZE);
+      namedWindow("Contours", CV_WINDOW_AUTOSIZE );
+      namedWindow( "gameboard contour", CV_WINDOW_AUTOSIZE );
+      namedWindow("Gameboard", CV_WINDOW_AUTOSIZE );
     }
 
-    namedWindow("Contours", CV_WINDOW_AUTOSIZE );
+    
   }
 
   ~board_detector()
     {
       if(debug_flag){
-        destroyWindow("Input_cutout");
-        destroyWindow("ROI-section"); 
-        destroyWindow("ROI-section-filtered");
+        destroyWindow("Contours");
+        destroyWindow("Input_cutout"); 
+        destroyWindow("gameboard contour");
+        destroyWindow("Gameboard");
       }    
     }
 
@@ -107,6 +97,10 @@ class board_detector
      
       // process the image
       org = cv_ptr->image;
+
+      if(debug_flag){
+      	imshow( "Input_cutout", org );
+      }
 
       //check if there is an image ...
       if(org.size().width==0 || org.size().height==0)
@@ -128,7 +122,6 @@ class board_detector
       if(debug_flag){
       	// Draw contours
         drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-
         countourtest;
         drawing.copyTo(countourtest);
 
@@ -140,20 +133,18 @@ class board_detector
 
         // Show in a window
         waitKey(10);
-        namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+        //namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
         imshow( "Contours", drawing );
       }
   
       //find lagrest countur (gameboard boundingbox)
       int largest_area=0;
       int largest_contour_index=0;
-      //Rect bounding_rect;
 
       // get bigges conure
 	  for( size_t i = 0; i< contours.size(); i++ ) // iterate through each contour.
 	    {
 	        double area = contourArea( contours[i] );  //  Find the area of contour
-
 	        if( area > largest_area )
 	        {
 	            largest_area = area;
@@ -164,32 +155,27 @@ class board_detector
 
 	  //output for sclicing up the thing
       Mat warpedCard(400, 400, CV_8UC3);
-
-      
-	  if(largest_area>org.rows*org.cols/1.5)
+      //check that the largest area is at least half of the image
+	  if(largest_area>org.rows*org.cols/2)
 		{
 		  if(debug_flag)
 		  {
 		    //draw onlz bigges countur
 		    Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 		    drawContours( countourtest, contours, largest_contour_index, color);
-		    namedWindow( "Contours2", CV_WINDOW_AUTOSIZE );
-		    imshow( "Contours2", countourtest );
+		    imshow( "gameboard contour", countourtest );
 		  }
-		  
 
-		  
-		  vector<Point> approx;
+		  vector<Point> corners;
 		  double d=0;
 		  do
 		  {
 		    d=d+1;
-		    approxPolyDP(contours[largest_contour_index],approx,d,true);
-		    //cout << approx.size() << " " <<d<<endl;
+		    approxPolyDP(contours[largest_contour_index],corners,d,true);
 		  }
-		  while (approx.size()>4);
+		  while (corners.size()>4);
 		  
-		  contours.push_back(approx);
+		  contours.push_back(corners);
 		  
 		  if(debug_flag){
 		  	Mat mc=org;
@@ -199,28 +185,44 @@ class board_detector
 		  
           //target points for homogentranform
 		  vector<Point2f> dest;
+		  dest.push_back(Point2f(warpedCard.cols, 0.0));
 		  dest.push_back(Point2f(0,0));
 		  dest.push_back(Point2f(0, warpedCard.rows));
 		  dest.push_back(Point2f(warpedCard.cols, warpedCard.rows));
-		  dest.push_back(Point2f(warpedCard.cols, 0.0));
 
           //inputpoints
+          //make sure approx and dest are in the right order ALWAZS
 		  vector<Point2f> inpoint;
-		  inpoint.push_back(approx[0]);
-		  inpoint.push_back(approx[1]);
-		  inpoint.push_back(approx[2]);
-		  inpoint.push_back(approx[3]);
+		  for(int j=0; j<corners.size();j++){
+		  	//check for first point
+		  	if(corners[j].x<warpedCard.cols/2 && corners[j].y<warpedCard.rows/2)
+		  	  inpoint.push_back(corners[j]);
+		  	
+		  	//check for second point
+		  	if(corners[j].x>warpedCard.cols/2 && corners[j].y<warpedCard.rows/2)
+		  	  inpoint.push_back(corners[j]);
+		  	
+		  	//check for 3 point
+		  	if(corners[j].x>warpedCard.cols/2 && corners[j].y>warpedCard.rows/2)
+		  	  inpoint.push_back(corners[j]);		  	
+		  	//check for 4 point
+		  	if(corners[j].x<warpedCard.cols/2 && corners[j].y>warpedCard.rows/2)
+		  	  inpoint.push_back(corners[j]);
+		  }
 
-		  //cout << "aprox size : " << approx.size() << endl;
-		  //cout << "p1: " << approx[0] << " p2: " << approx[1] << " p3: " << approx[2] << " p4: " << approx[3] << endl;
-		  if (approx.size() == 4)
+		  if(debug_flag){
+		  	cout << "aprox size : " << inpoint.size() << endl;
+		    cout << "p1: " << inpoint[0] << " p2: " << inpoint[1] << " p3: " << inpoint[2] << " p4: " << inpoint[3] << endl;
+		  }
+		  
+		  if (corners.size() == 4)
 		    {
 		      Mat homography = findHomography(inpoint, dest);
 		      warpPerspective(org, warpedCard, homography, Size(warpedCard.cols, warpedCard.rows));
 		    }
             
             if(debug_flag){
-            	imshow("warped card", warpedCard);
+            	imshow("Gameboard", warpedCard);
             }
 
             //slize it up!!!
@@ -239,15 +241,7 @@ class board_detector
 			  Rec.y=image_height/rows*rowcount;
 			  Rec.width=image_width/cols;
 			  Rec.height=image_height/rows;
-			  //keep track of ROI
-			  colcount+=1;
-			  if (colcount>cols-1){
-			    colcount=0;
-			    rowcount+=1;
-			    if (rowcount>rows-1){
-			      rowcount=0;
-			    }
-			  }
+			  
 			  //copy to new sub image 
 			  subImage = warpedCard(Rec).clone();
 			  waitKey(10);
@@ -258,56 +252,43 @@ class board_detector
 				imshow("Step show sub", subImage);
 				waitKey(100);
 			  }   
-						    
-			  //filter for red
-			  inRange(subImage,   cv::Scalar(filter_red_b_low, filter_red_g_low, filter_red_r_low), cv::Scalar(filter_red_b_high, filter_red_g_high, filter_red_r_high), filter_image_red);
-			  waitKey(10);
-			  //filter blue
-			  inRange(subImage,   cv::Scalar(filter_blue_b_low, filter_blue_g_low, filter_blue_r_low), cv::Scalar(filter_blue_b_high, filter_blue_g_high, filter_blue_r_high), filter_image_blue);
-			  waitKey(10);
-			  //debug show filter image
+
+			  //get mean color of ROI
+			  Scalar meancolor = cv::mean(subImage);
 			  if(debug_flag){
-				namedWindow("show filter sub", WINDOW_AUTOSIZE);
-				imshow("Step show filterv sub", subImage);
-				waitKey(100);
-				namedWindow("show filter sub", WINDOW_AUTOSIZE);
-				imshow("Step show filterv sub", filter_image_red);
-				waitKey(100);
-			    namedWindow("show filter sub", WINDOW_AUTOSIZE);
-				imshow("Step show filterv sub", subImage);
-				waitKey(100);
-				namedWindow("show filter sub", WINDOW_AUTOSIZE);
-				imshow("Step show filterv sub", filter_image_blue);
-				waitKey(100);
+			    cout << "row: " << rowcount << " col: " << colcount << " R: " << meancolor[0] << " G: " << meancolor [1] << " B: " << meancolor[2] << endl;
 			  }
-				
-			  //make bw version
-			  Mat bw_red=filter_image_red>128;
-			  Mat bw_blue=filter_image_blue>128;
-			  //count white pixel
-			  double white_red= countNonZero(bw_red);
-			  double white_blue= countNonZero(bw_blue);
-			  double allpix=filter_image_red.size().width*filter_image_red.size().height;
-			  //if more than 5% is red and les then 1% blue -> its O-piece
-			  if(white_red>allpix/95 && white_blue<allpix/99 ){
-			    //ROS_INFO_STREAM( " O-piece detected" );
+			  			    
+			  // what piece is it?
+			  if(meancolor[0]<r_threshold && meancolor[2]>b_threshold ){
+			    //ROS_INFO_STREAM( " Red-piece detected" );
 			    gameboard[rowcount][colcount]=2;
 			  }
-			  //if more than 5% is blue and les then 1% red -> its X-piece	    
-			  if(white_red<allpix/99 && white_blue>allpix/95 ){
-			    //ROS_INFO_STREAM( " X-piece detected" );	
+			  if(meancolor[0]>r_threshold && meancolor[2]<b_threshold ){
+			    //ROS_INFO_STREAM( " Blue-piece detected" );	
 				gameboard[rowcount][colcount]=1;
 			  }
 			  //if less than 1% is red and les then 1% blue -> its no-piece	    
-			  if(white_red<allpix/99 && white_blue<allpix/99 ){
+			  if(meancolor[0]>r_threshold && meancolor[2]>b_threshold ){
 			    //ROS_INFO_STREAM( " No-piece detected" );	
 				gameboard[rowcount][colcount]=0;
 			  }
+			  //keep track of ROI
+			  colcount+=1;
+			  if (colcount>cols-1){
+			    colcount=0;
+			    rowcount+=1;
+			    if (rowcount>rows-1){
+			      rowcount=0;
+			    }
+			  }
+
 			 waitKey(10);
 				
 		    }
 
 		    //Print gameboard
+		    cout << "*********************************" << endl;
 		    for(int i=0;i<rows;i++){
 			  for(int j=0;j<cols;j++){
 				cout << "|" << gameboard[i][j];
@@ -315,6 +296,7 @@ class board_detector
 			  cout << "|" << endl;
 			  cout << "_______________" << endl;		
 		    }
+		    cout << "*********************************" << endl;
 		  
 	    }
     }
@@ -333,159 +315,4 @@ int main(int argc, char** argv)
   ros::spin();
   return 0;
 }
-
-/*
-
-int main(int argc, char **argv)
-{
-
-//TODO: make board detection into service
- //ROS init
-	ros::init(argc, argv, "board_detector");
-	ros::NodeHandle n;
-    ros::Rate loop_rate(5);
-    ROS_INFO_THROTTLE(1 , "starting");
-    loop_rate.sleep();
-
- //var init
-    //gameboard    
-    int rows=7;
-    int cols=7;
-    //0 = no piece
-    //1 = X-piece
-    //2 = O-piece
-    int** gameboard = new int*[rows];
-	for(int i = 0; i < rows; ++i)
-    	gameboard[i] = new int[cols];
- 
-	//  create images for processing
-	Mat LoadedImage;
-	Mat subImage;
-	Mat filter_image_red;
-	Mat filter_image_blue;
-	//TODO: load from stream
-	LoadedImage = imread("/home/tigger77/catkin_ws/src/board_detector/src/mokttt.jpg", IMREAD_COLOR);
- 	
- 	/*
- 	//debug show loaded image
-	namedWindow("Step 1 image loaded", WINDOW_AUTOSIZE);
-	imshow("Step 1 image loaded", LoadedImage);
-	waitKey(100);
-    
-
-	/// parameters
-	int rowcount=0;
-	int colcount=0;
-	int image_width=LoadedImage.size().width;
-	int image_height=LoadedImage.size().height;
-	int filter_red_r_low=50;
-	int filter_red_g_low=0;
-	int filter_red_b_low=0;
-	int filter_red_r_high=255;
-	int filter_red_g_high=25;
-	int filter_red_b_high=25;
-	int filter_blue_r_low=0;
-	int filter_blue_g_low=0;
-	int filter_blue_b_low=50;
-	int filter_blue_r_high=25;
-	int filter_blue_g_high=25;
-	int filter_blue_b_high=255;
-	Rect Rec(0, 0, 10, 10);
-
-	//Loop through all ROI and se if its O or X
-	for(int i=0;i<cols*rows;i++){
-	    //ROI
-	    Rec.x=image_width/cols*colcount;
-	    Rec.y=image_height/rows*rowcount;
-	    Rec.width=image_width/cols;
-	    Rec.height=image_height/rows;
-	    //keep track of ROI
-	    colcount+=1;
-	    if (colcount>cols-1){
-	     	colcount=0;
-	        rowcount+=1;
-	        if (rowcount>rows-1){
-		     	rowcount=0;
-		    }
-	    }
-	    //copy to new sub image 
-		subImage = LoadedImage(Rec).clone();
-		waitKey(10);
-
-		//debug subimages  
-		/*   
-		namedWindow("show sub", WINDOW_AUTOSIZE);
-		imshow("Step show sub", subImage);
-		waitKey(100);
-	    	
-	    
-	    //filter for red
-	    inRange(subImage,   cv::Scalar(filter_red_b_low, filter_red_g_low, filter_red_r_low), cv::Scalar(filter_red_b_high, filter_red_g_high, filter_red_r_high), filter_image_red);
-	    waitKey(10);
-	    //filter blue
-		inRange(subImage,   cv::Scalar(filter_blue_b_low, filter_blue_g_low, filter_blue_r_low), cv::Scalar(filter_blue_b_high, filter_blue_g_high, filter_blue_r_high), filter_image_blue);
-	    waitKey(10);
-	    //debug show filter image
-	    /*
-	    namedWindow("show filter sub", WINDOW_AUTOSIZE);
-		imshow("Step show filterv sub", subImage);
-		waitKey(1000);
-		namedWindow("show filter sub", WINDOW_AUTOSIZE);
-		imshow("Step show filterv sub", filter_image_red);
-		waitKey(1000);
-	    namedWindow("show filter sub", WINDOW_AUTOSIZE);
-		imshow("Step show filterv sub", subImage);
-		waitKey(1000);
-		namedWindow("show filter sub", WINDOW_AUTOSIZE);
-		imshow("Step show filterv sub", filter_image_blue);
-		waitKey(1000);
-		
-		
-		//make bw version
-	    Mat bw_red=filter_image_red>128;
-	    Mat bw_blue=filter_image_blue>128;
-	    //count white pixel
-		double white_red= countNonZero(bw_red);
-		double white_blue= countNonZero(bw_blue);
-	    double allpix=filter_image_red.size().width*filter_image_red.size().height;
-	    //if more than 5% is red and les then 1% blue -> its O-piece
-	    if(white_red>allpix/95 && white_blue<allpix/99 ){
-	       ROS_INFO_STREAM( " O-piece detected" );
-	       gameboard[rowcount][colcount]=2;
-		}
-		//if more than 5% is blue and les then 1% red -> its X-piece	    
-		if(white_red<allpix/99 && white_blue>allpix/95 ){
-		   ROS_INFO_STREAM( " X-piece detected" );	
-		   gameboard[rowcount][colcount]=1;
-		}
-		//if less than 1% is red and les then 1% blue -> its no-piece	    
-		if(white_red<allpix/99 && white_blue<allpix/99 ){
-		   ROS_INFO_STREAM( " No-piece detected" );	
-		   gameboard[rowcount][colcount]=0;
-		}
-		waitKey(10);
-		
-	}
-
-	//Print gameboard
-	for(int i=0;i<rows;i++){
-		for(int j=0;j<cols;j++)
-		{
-			cout << "|" << gameboard[i][j];
-		}
-		cout << "|" << endl;
-		cout << "_______________" << endl;
-		
-	}
- 
- while (ros::ok())
-  {
-  	ROS_INFO_THROTTLE(1 , "Waiting");
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-
-
-}*/
-
 
